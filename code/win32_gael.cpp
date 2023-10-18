02c0f58ff8bc456d5068933533d5f7e9efa6a188
Eelis Rossi <eelis.rossi@gmail.com>
2023-10-18 20:48:19 +0300

    finished day 3, builds w/o errors

--- code/win32_gael.cpp
+++ code/win32_gael.cpp
@@ -1,103 +1,156 @@
 #include <windows.h>
+#include <wingdi.h>
 
-LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam,
-                                    LPARAM LParam)
+#define internal static
+#define local_persist static
+#define global_variable static
+
+// TODO: This is a global for now.
+global_variable bool Running;
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void
+Win32ResizeDIBSection(int Width, int Height)
+{
+    // TODO: Bulletproof this.
+    // Maybe don't free first, free after, then free first if that fails.
+
+    if (BitmapHandle) {
+        DeleteObject(BitmapHandle);
+    } 
+
+    if (!BitmapDeviceContext)
+    {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    BitmapHandle = CreateDIBSection(
+      BitmapDeviceContext, &BitmapInfo, DIB_RGB_COLORS, &BitmapMemory, 0, 0);
+}
+
+internal void
+Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+    StretchDIBits(
+      DeviceContext,
+      X,
+      Y,
+      Width,
+      Height,
+      X,
+      Y,
+      Width,
+      Height,
+      BitmapMemory,
+      &BitmapInfo,
+      DIB_RGB_COLORS,
+      SRCCOPY);
+}
+
+LRESULT CALLBACK
+Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
 {
     LRESULT Result = 0;
 
-    switch (Message)
-    {
-    case WM_SIZE:
-    {
-        OutputDebugStringA("WM_SIZE");
-    }
-    break;
+    switch (Message) {
+        case WM_SIZE: {
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            int Width = ClientRect.right - ClientRect.left;
+            int Height = ClientRect.bottom - ClientRect.top;
+            Win32ResizeDIBSection(Width, Height);
+        } break;
 
-    case WM_DESTROY:
-    {
-        OutputDebugStringA("WM_DESTROY");
-        PostQuitMessage(0);
-    }
-    break;
+        case WM_DESTROY: {
+            // TODO: Handle this as an error - recreate window?
+            Running = false;
+        } break;
 
-    case WM_CLOSE:
-    {
-        OutputDebugStringA("WM_CLOSE");
-        PostQuitMessage(0);
-    }
-    break;
+        case WM_CLOSE: {
+            // TODO: Handle this with a message to the user?
+            Running = false;
+        } break;
 
-    case WM_ACTIVATEAPP:
-    {
-        OutputDebugStringA("WM_ACTIVATEAPP");
-    }
-    break;
+        case WM_ACTIVATEAPP: {
+            OutputDebugStringA("WM_ACTIVATEAPP");
+        } break;
 
-    case WM_PAINT:
-    {
-        PAINTSTRUCT Paint;
-        HDC DeviceContext = BeginPaint(Window, &Paint);
-        int X = Paint.rcPaint.left;
-        int Y = Paint.rcPaint.top;
-        LONG Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-        LONG Width = Paint.rcPaint.right - Paint.rcPaint.left;
-        PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
-        EndPaint(Window, &Paint);
-    }
-    break;
+        case WM_PAINT: {
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            LONG Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            LONG Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+            EndPaint(Window, &Paint);
+        } break;
 
-    default:
-    {
-        Result = DefWindowProc(Window, Message, WParam, LParam);
-    }
-    break;
+        default: {
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
     }
 
     return (Result);
 }
 
-int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance,
-                     LPSTR CommandLine, int ShowCode)
+int CALLBACK
+WinMain(
+  HINSTANCE Instance,
+  HINSTANCE PrevInstance,
+  LPSTR CommandLine,
+  int ShowCode)
 {
     WNDCLASS WindowClass = {};
 
     // TODO: video might modify this later
     WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.lpfnWndProc = Win32MainWindowCallback;
     WindowClass.hInstance = Instance;
     // WindowClass.hIcon;
     WindowClass.lpszClassName = "GaelEngineWindowClass";
 
-    if (RegisterClass(&WindowClass))
-    {
+    if (RegisterClass(&WindowClass)) {
         HWND WindowHandle = CreateWindowEx(
-            0, WindowClass.lpszClassName, "GaelEngine",
-            WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
-            CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, Instance, 0);
-        if (WindowHandle)
-        {
-            for (;;)
-            {
+          0,
+          WindowClass.lpszClassName,
+          "GaelEngine",
+          WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+          CW_USEDEFAULT,
+          CW_USEDEFAULT,
+          CW_USEDEFAULT,
+          CW_USEDEFAULT,
+          0,
+          0,
+          Instance,
+          0);
+        if (WindowHandle) {
+            Running = true;
+            while (Running) {
                 MSG Message;
                 BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
-                if (MessageResult > 0)
-                {
+                if (MessageResult > 0) {
                     TranslateMessage(&Message);
                     DispatchMessageA(&Message);
-                }
-                else
-                {
+                } else {
                     break;
                 }
             }
-        }
-        else
-        {
+        } else {
             // TODO: Logging
         }
-    }
-    else
-    {
+    } else {
         // TODO: logging
     }
 
